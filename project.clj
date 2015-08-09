9fe8da31bf5dffeafaf6288f2ef34829d193140b
Gary Fredericks <fredericksgary@gmail.com>
2015-08-09 12:11:21 -0500

    Lein 2.5.2 fixes the `lein test` problem

--- project.clj
+++ project.clj
@@ -7,10 +7,6 @@
                  [org.clojure/clojurescript "0.0-3308"]
                  [com.gfredericks/goog-integer "1.0.0"]]
 
-  ;; temporary hack until https://github.com/technomancy/leiningen/issues/1940
-  ;; is fixed
-  :aliases {"test" ["test" "com.gfredericks.exact-test"]}
-
   :plugins [[lein-cljsbuild "1.0.6"]]
   :cljsbuild
   {:builds
