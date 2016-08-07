0dfbc74f0eec764886a017bb6fa36c2b7112c58c
Gary Fredericks <fredericksgary@gmail.com>
2016-08-07 18:54:03 -0500

    Upgrade everything

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/gfredericks/exact"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.48"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.89"]
                  [com.gfredericks/goog-integer "1.0.0"]]
 
-  :plugins [[lein-cljsbuild "1.0.6"]]
+  :plugins [[lein-cljsbuild "1.1.3"]]
   :cljsbuild
   {:builds
    [{:id "node-dev"
