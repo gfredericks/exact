b98823e3819cc45b01f0cfac9fcdfb9e3574368f
Gary Fredericks <fredericksgary@gmail.com>
2016-08-14 16:12:20 -0500

    Version 0.1.10-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/exact "0.1.9"
+(defproject com.gfredericks/exact "0.1.10-SNAPSHOT"
   :description "Portable exact arithmetic in Clojure"
   :url "https://github.com/gfredericks/exact"
   :license {:name "Eclipse Public License"
