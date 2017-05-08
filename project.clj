5989319bd043554a531c6f33d0cb75300c9dc923
Gary Fredericks <gfredericks@drw.com>
2017-05-07 20:36:44 -0500

    Upgrade clojurescript
    This is the latest version with no issues

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.89"]
+                 ;; cljs version 473 has compile warnings,
+                 ;; and with 521 the tests won't run at all
+                 [org.clojure/clojurescript "1.9.456"]
                  [com.gfredericks/goog-integer "1.0.1"]]
 
   :plugins [[lein-cljsbuild "1.1.6"]]
