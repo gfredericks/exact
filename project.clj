4f9259956b089c8e656ea1e02c9ee4b9831a30bc
Gary Fredericks <gfredericks@drw.com>
2017-07-08 20:04:55 -0500

    Upgrade clojurescript, adjust test runner

--- project.clj
+++ project.clj
@@ -4,24 +4,22 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 ;; cljs version 473 has compile warnings,
-                 ;; and with 521 the tests won't run at all
-                 [org.clojure/clojurescript "1.9.456"]
+                 [org.clojure/clojurescript "1.9.671"]
                  ;; need at least this version to avoid a couple
                  ;; Integer bugs
                  [org.clojure/google-closure-library "0.0-20170519-fa0499ef"]]
-
   :plugins [[lein-cljsbuild "1.1.6"]]
   :cljsbuild
   {:builds
    [{:id "node-dev"
      :source-paths ["src" "test"]
-     :notify-command ["node" "resources/run.js"]
+     :notify-command ["node" "target/cljs/node_dev/tests.js"]
      :compiler {:optimizations :none
                 :static-fns true
                 :target :nodejs
                 :output-to "target/cljs/node_dev/tests.js"
                 :output-dir "target/cljs/node_dev/out"
+                :main com.gfredericks.exact-test-main
                 :source-map true}}]}
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
   :aliases {"circle-ci"
